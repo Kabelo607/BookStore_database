b412e5842b926d06fe8a748638f7af486800b357
AndiswaCyria <andiswacyriam@gmail.com>
2025-04-12 20:22:21 +0200

    Books data entry

---
+++ data/sample_authors.sql
@@ -0,0 +1,9 @@
+INSERT INTO author (name) VALUES
+('Chinua Achebe'),
+('Chimamanda Ngozi Adichie'),
+('Ngugi wa Thiong\'o'),
+('Tsitsi Dangarembga'),
+('Sefi Atta'),
+('Wole Soyinka'),
+('Lola Shoneyin'),
+('Mohale Mashigo');
